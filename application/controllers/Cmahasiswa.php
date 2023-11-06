b460a65ad95b8831e4ea338938e95dd9f876080f
Anastasya <work.anastasyaa@gmail.com>
2023-11-06 08:40:35 +0700

    p

--- application/controllers/Cmahasiswa.php
+++ application/controllers/Cmahasiswa.php
@@ -1,12 +1,12 @@
 <?php
 
 class Cmahasiswa extends CI_Controller{
-	// public function __construct()
-	// 	{
-	// 		parent::__construct();
-	// 		$this->load->model('mvalidasi');
-	// 		$this->mvalidasi->validasi();
-	// 	}
+	public function __construct()
+		{
+			parent::__construct();
+			// $this->load->model('mvalidasi');
+			// $this->mvalidasi->validasi();
+		}
 	
 	public function dashboard(){
 		$data=['judul'=>'dashboard'];
