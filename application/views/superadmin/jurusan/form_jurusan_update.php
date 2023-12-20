960bf5c2beb68f84ae4e1f81bb9286507bd357fb
imanuel22 <imanuelrenra220@gmail.com>
2023-12-20 14:21:44 +0800

    update ui

--- application/views/superadmin/jurusan/form_jurusan_update.php
+++ application/views/superadmin/jurusan/form_jurusan_update.php
@@ -4,19 +4,19 @@
 <form action="<?= base_url('csuperadmin/update_jurusan')?>" method="post">
 <input type="hidden" name="id_jurusan" value="<?=$data_jurusan_id->id_jurusan?>">
 		<div class="mb-3">
-			<label for="nama_jurusan" class="form-label">nama_jurusan</label>
+		<label for="nama_jurusan" class="form-label text-light">Nama Jurusan</label>
 			<input type="text" name="nama_jurusan" class="form-control bg-light" id="nama_jurusan" value="<?=$data_jurusan_id->nama_jurusan?>">
 		</div>
 		<div class="mb-3">
-			<label for="NoSKJurusan" class="form-label">NoSKJurusan</label>
+		<label for="NoSKJurusan" class="form-label text-light">NoSk. Jurusan</label>
 			<input type="text" name="NoSKJurusan" class="form-control bg-light" id="NoSKJurusan" value="<?=$data_jurusan_id->NoSKJurusan?>">
 		</div>
 		<div class="mb-3">
-			<label for="Kajur" class="form-label">Kajur</label>
+		<label for="Kajur" class="form-label text-light">Kepala Jurusan</label>
 			<input type="text" name="Kajur" class="form-control bg-light" id="Kajur" value="<?=$data_jurusan_id->Kajur?>">
 		</div>
 		<div class="mb-3">
-			<label for="keterangan" class="form-label">keterangan</label>
+		<label for="keterangan" class="form-label text-light">Keterangan</label>
 			<input type="text" name="keterangan" class="form-control bg-light" id="keterangan" value="<?=$data_jurusan_id->keterangan?>">
 		</div>	
 		<div class="mb-3 row">
