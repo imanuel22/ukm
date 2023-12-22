f34854a2d790c2113e30cb0370477301a1d19f69
imanuel22 <imanuelrenra220@gmail.com>
2023-12-23 02:10:55 +0800

    ui anggota

--- application/views/fungsionaris/anggota_ukm/anggota_insert.php
+++ application/views/fungsionaris/anggota_ukm/anggota_insert.php
@@ -1,10 +1,11 @@
-<div class="bg-info rounded-3 text-light p-3">
-    <h1>tambah anggota</h1><hr>
+<h3 class="mb-4">Form Tambah Fungsionaris</h3>
+<div class="card bg-primary ">
+	<div class="card-body">
     <form action="<?=base_url('cfungsionaris/insert_anggota')?>" method="post">
     <input type="hidden" name="id_ukm" value="<?=$id_ukm?>">
     <div class="mb-3">
-			<label for="id_mahasiswa" class="form-label">nama mahasiswa</label>
-			<select name="id_mahasiswa" id="id_mahasiswa" class="form-control">
+			<label for="id_mahasiswa" class="form-label text-light">nama mahasiswa</label>
+			<select name="id_mahasiswa" id="id_mahasiswa" class="form-control bg-light">
 				<option value="" hidden>pilih</option>
 				<?php foreach($data_mahasiswa as $row):?>
 				<option value="<?=$row->id_mahasiswa?>"><?=$row->nama_mahasiswa?></option>
@@ -12,21 +13,23 @@
 			</select>
 	</div>	
     <div class="mb-3">
-			<label for="id_devisi" class="form-label">devisi</label>
-			<select name="id_devisi" id="id_devisi" class="form-control">
+			<label for="id_devisi" class="form-label text-light">devisi</label>
+			<select name="id_devisi" id="id_devisi" class="form-control bg-light">
 				<option value="" hidden>pilih</option>
 				<?php foreach($data_devisi as $row):?>
 				<option value="<?=$row->id_devisi?>"><?=$row->nama_devisi?></option>
 				<?php endforeach;?>
 			</select>
 	</div>	
-    <div class="mb-3 row">
-        <div class="col-6">
-            <button class="btn btn-primary col-12" type="submit">SUBMIT</button>
-        </div>
-        <div class="col-6">
-            <button class="btn btn-danger col-12" type="reset">reset</button>
-        </div>
-    </div>
-    </form>
-</div>
+	<div class="mb-3 row">
+			<div class="col-6">
+				<button type="submit" class="btn btn-success col-12">Submit</button>
+			</div>
+			<div class="col-6">
+				<button type="reset" class="btn btn-danger col-12">Reset</button>
+			</div>
+		</div>
+	</form>
+	</div>
+</div>	
+<hr class="border border-primary border-2 opacity-50">
