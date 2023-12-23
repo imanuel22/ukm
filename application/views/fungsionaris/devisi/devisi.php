890d93886d02e58454bc3bb60af13e6d37966c6f
imanuel22 <imanuelrenra220@gmail.com>
2023-12-23 19:56:13 +0800

    Update devisi.php

--- application/views/fungsionaris/devisi/devisi.php
+++ application/views/fungsionaris/devisi/devisi.php
@@ -1,24 +1,20 @@
 <link rel="stylesheet" href="<?=base_url();?>assets/DataTables/datatables.css">
-<div class="rounded-4 p-4 bg-info mt-3">
-	<div class="row mb-2">
-		<div class="col-10">
-			<h1 class="fw-bold ">Table ukm</h1>
-		</div>
-		<div class="col-2">
-			<button type="button" onclick="tambah(<?=$id_ukm?>)" class="btn btn-primary p-1 col-12">+Data</button>
-		</div>
+<h3 class="mb-4">Table Devisi</h3>
+<div class="card bg-primary mt-3 text-light">
+	<div class="card-header d-flex justify-content-end">
+		<button type="button" onclick="tambah(<?=$id_ukm?>)" class="btn btn-light px-5">+Data</button>
 	</div>
-	<div  style="overflow-x:auto;">
-
-<table id="myTable" class="table display table-warning table-hover table-responsive">
-    <thead>
-        <tr>
-            <th>no</th>
-            <th>nama_devisi</th>
-            <th>Aktion</th>
+	<div class="card-body">
+		<div style="overflow-x:scroll;">
+			<table id="myTable" class="table table-bordered display table-striped ">
+				<thead class="table-light">
+					<tr>
+						<th class="text-center">No</th>
+            <th class="text-center">nama_devisi</th>
+            <th class="text-center">Aktion</th>
         </tr>
     </thead>
-    <tbody>
+    <tbody class="bg-light">
         <?php 
 		$no=1;
 		foreach($data_devisi as $row):
@@ -26,9 +22,9 @@
 			<tr>
 				<td><?=$no++?></td>
 				<td><?=$row->nama_devisi?></td>
-				<td>
-					<button type="button" class="btn btn-warning" onclick="edit(<?=$id_ukm?>,<?=$row->id_devisi?>)"><i class="bi bi-pencil"></i></button>
-					<button type="button" class="btn btn-danger" onclick="hapus(<?=$id_ukm?>,<?=$row->id_devisi?>)"><i class="bi bi-trash3"></i></button>
+				<td class="text-center">
+					<button type="button" class="btn btn-warning" onclick="edit(<?=$id_ukm?>,<?=$row->id_devisi?>)"><i class="ti ti-pencil"></i></button>
+					<button type="button" class="btn btn-danger" onclick="hapus(<?=$id_ukm?>,<?=$row->id_devisi?>)"><i class="ti ti-trash"></i></button>
 				</td>
 			</tr>
 		<?php endforeach;?>
