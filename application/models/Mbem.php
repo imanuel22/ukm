50d0c50418f68ad4196c562666a262b6ea463d50
imanuel22 <imanuelrenra220@gmail.com>
2023-12-24 19:11:39 +0800

    Update Mbem.php

--- application/models/Mbem.php
+++ application/models/Mbem.php
@@ -10,6 +10,7 @@ class Mbem extends CI_Model{
 		return $query->row();
 		
 	}
+
 	public function proses_bem(){
 		$data = [
 			'id_mahasiswa' => $this->input->post('id_mahasiswa'),
@@ -48,12 +49,12 @@ class Mbem extends CI_Model{
 					'max_size'=>0,	
 					'file_name'=>$file_name,
 				];
-			$this->load->library('upload',$config);
-			
-			$target_file ='assets/uploads/img_mahasiswa/'.$this->input->post('img_mahasiswa_old');
-			unlink($target_file);
-			$this->upload->do_upload('img_mahasiswa');
-			$data['img_mahasiswa']=$this->upload->data('file_name');
+				$this->load->library('upload',$config);
+				
+				$target_file ='assets/uploads/img_mahasiswa/'.$this->input->post('img_mahasiswa_old');
+				unlink($target_file);
+				$this->upload->do_upload('img_mahasiswa');
+				$data['img_mahasiswa']=$this->upload->data('file_name');
 			}else{
 				$data['img_mahasiswa']=$this->input->post('img_mahasiswa_old');
 			}
@@ -85,7 +86,7 @@ class Mbem extends CI_Model{
 			echo "<script>$('#status').val('".$data->status."')</script>";
 			echo "<script>$('#id_prodi').val('".$data->id_prodi."')</script>";
 		}	
-}
+	}
 
 	public function delete_bem($id_mahasiswa){
 		$query=$this->get_bem_id($id_mahasiswa);
@@ -100,7 +101,5 @@ class Mbem extends CI_Model{
 		}
 		redirect(base_url('csuperadmin/bem'),'_self');
 	}
-	
 }
-
 ?>
