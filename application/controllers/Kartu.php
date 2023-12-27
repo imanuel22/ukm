6ebcaddacaa0638341065232b0be5117d15168ca
Anastasya <work.anastasyaa@gmail.com>
2023-12-27 13:07:53 +0800

    Create Kartu.php

---
+++ application/controllers/Kartu.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+    function cetakpdf()
+		{
+			$data['hasil']=$this->mprodi->tampildata();
+
+			require_once(APPPATH . 'libraries/dompdf/autoload.inc.php');
+			$pdf = new Dompdf\Dompdf();
+			$pdf->setPaper('A6', 'landscape');
+			$pdf->set_option('isRemoteEnabled', TRUE);
+			$pdf->set_option('isHtml5ParserEnabled', true);
+			$pdf->set_option('isPhpEnabled', true);
+			$pdf->set_option('isFontSubsettingEnabled', true);
+			
+			$pdf->loadHtml($this->load->view('cetak_pdf',$data, true));
+			$pdf->render();
+			$pdf->stream('NamaFile', ['Attachment' => false]);	
+            
+		}
+?>
