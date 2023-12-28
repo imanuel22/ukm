a2b0d92d29b82414addc91037507955a73aa8785
imanuel22 <imanuelrenra220@gmail.com>
2023-12-28 16:11:52 +0800

    fitur ajax register

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -3,6 +3,9 @@
 
 <head>
     <title>Register</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/styles.min.css" />
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
@@ -74,14 +77,19 @@
                         <input type="text" class="form-control" id="no_telp" name="no_telp" required>
                     </div>
                     <div class="mb-3">
-                        <label for="id_prodi" class="form-label">ID Prodi:</label>
-                        <select class="form-control" name="id_prodi" id="id_prodi" required>
-                            <option value="0" hidden>pilih</option>
+                        <label for="id_jurusan" class="form-label">Jurusan:</label>
+                        <select class="form-control" name="id_jurusan" id="id_jurusan" required>
+                            <option value="" hidden>Pilih Jurusan</option>
                             <?php 
-                            foreach($data_prodi as $row):
+                            foreach($data_jurusan as $row):
                             ?>
-                            <option value="<?=$row->id_prodi?>"><?=$row->nama_prodi?></option>
+                            <option value="<?=$row->id_jurusan?>"><?=$row->nama_jurusan?></option>
                             <?php endforeach;?>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="id_prodi" class="form-label">Prodi:</label>
+                        <select class="form-control" name="id_prodi" id="id_prodi" required>
                         </select>
                     </div>
                     <div class="mb-3">
@@ -104,14 +112,43 @@
         function login() {
             window.open("<?= base_url('chome/login')?>", "_self");
         }
+        
+        $(document).ready(function(){
+            $('#id_jurusan').change(function() {
+                var getJurusanID = $('#id_jurusan').val();
+                
+                $.ajax({
+                    type: 'POST',
+                    dataType: 'JSON',
+                    url: '<?=base_url()?>cauth/getprodi',
+                    data: {id_jurusan:getJurusanID},
+                    success: function(data){
+                        console.log(data);
+                        var html = "<option hidden value=''>Pilih Prodi</option>";
+                        var i;
+                        for (i = 0; i < data.length; i++) {
+                            html +='<option value="'+data[i].id_prodi+'">'+data[i].nama_prodi+'</option>';                        
+                        }
+                        $('#id_prodi').html(html);
+                    }
+                })
+            })
+        })
     </script>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-        crossorigin="anonymous"></script>
-</body>
+<script src="<?=base_url()?>assets/libs/jquery/dist/jquery.min.js"></script>
+<script src="<?=base_url()?>assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?=base_url()?>assets/js/sidebarmenu.js"></script>
+<script src="<?=base_url()?>assets/js/app.min.js"></script>
+<script src="<?=base_url()?>assets/libs/simplebar/dist/simplebar.js"></script>
 
+<!-- ini untuk memanggil file yang ada pada folder jquery-->       
+<script src="<?php echo base_url(); ?>assets/jquery/jquery.js"></script>
+        <script src="<?php echo base_url(); ?>assets/jquery/app.js"></script>
+        <script language="javascript">
+			var site = "<?php echo base_url()?>index.php/";
+			var loading_image_large = "<?php echo base_url()?>assets/gambar/loading_large.gif";
+		</script>
+</body>
+<div id="script"></div>
 </html>
