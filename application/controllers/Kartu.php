31e077ee8a1610edd5804cfe6549ec013460f6f2
imanuel22 <imanuelrenra220@gmail.com>
2023-12-30 21:17:17 +0800

    a

--- application/controllers/Kartu.php
+++ application/controllers/Kartu.php
@@ -1,13 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Kartu extends CI_Controller{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('mcard');
+		require_once(APPPATH . 'libraries/dompdf/autoload.inc.php');	
+	}
     function printcardf($id_mahasiswa,$id_ukm)
 		{
-			$this->load->model('mcard');
 			$data1['data_cardF']=$this->mcard->card_fungsionaris_ukm($id_mahasiswa,$id_ukm);
-
-			require_once(APPPATH . 'libraries/dompdf/autoload.inc.php');
 			$pdf = new Dompdf\Dompdf();
+
 			$pdf->setPaper(array(0,0,400 ,207 ), 'portrait');
 			$pdf->set_option('isRemoteEnabled', TRUE);
 			$pdf->set_option('isHtml5ParserEnabled', true);
@@ -21,17 +25,14 @@ class Kartu extends CI_Controller{
 		}
 		function printcarda($id_mahasiswa,$id_ukm)
 		{
-			$this->load->model('mcard');
+			
 			$data1['data_cardA']=$this->mcard->card_anggotaUKM_ukm($id_mahasiswa,$id_ukm);
-
-			require_once(APPPATH . 'libraries/dompdf/autoload.inc.php');
 			$pdf = new Dompdf\Dompdf();
 			$pdf->setPaper(array(0,0,400 ,207 ), 'portrait');
 			$pdf->set_option('isRemoteEnabled', TRUE);
 			$pdf->set_option('isHtml5ParserEnabled', true);
 			$pdf->set_option('isPhpEnabled', true);
 			$pdf->set_option('isFontSubsettingEnabled', true);
-			
 			$pdf->loadHtml($this->load->view('mahasiswa/cetakida',$data1, true));
 			$pdf->render();
 			$pdf->stream('ID Card UKM', ['Attachment' => false]);	
