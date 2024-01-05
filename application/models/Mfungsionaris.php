0f38e6e83f4a68e2d5884cba4fcf1a58899b0785
imanuel22 <imanuelrenra220@gmail.com>
2024-01-05 17:24:44 +0800

    Update Mfungsionaris.php

--- application/models/Mfungsionaris.php
+++ application/models/Mfungsionaris.php
@@ -1,15 +1,19 @@
 <?php
     class Mfungsionaris extends CI_Model {
-        public function get_fungsionaris($id_ukm){
+	//mengambila data ke view fungsionaris dari id_ukm = parameter
+    public function get_fungsionaris($id_ukm){
             $query = $this->db->get_where('fungsionaris',['id_ukm'=>$id_ukm]);
             return $query->result();
-        }public function get_fungsionaris_id($id_fungsionaris){
+        }	
+        //mengambila data ke tb_fungsionaris dari id_fungsionaris = parameter 
+        public function get_fungsionaris_id($id_fungsionaris){
             $query = $this->db->get_where('tb_fungsionaris',['id_fungsionaris'=>$id_fungsionaris]);
             return $query->row();
         }
-        public function proses_fungsionaris(){
-            $pesan = '';
-            $color = '';
+        
+	//function buat insert + update data 
+    public function proses_fungsionaris(){
+            
             $data= [
                 'id_fungsionaris'=>$this->input->post('id_fungsionaris'),
                 'id_mahasiswa'=>$this->input->post('id_mahasiswa'),
@@ -17,15 +21,17 @@
                 'status'=>$this->input->post('status'),
             ];
             $id_fungsionaris=$data['id_fungsionaris'];
-            if(empty($id_fungsionaris)){
-                $this->db->insert('tb_fungsionaris',$data);
+		//cek jika tidak ada id_fungsionaris lakukan insert
+        if(empty($id_fungsionaris)){
+			//menambahkan data ke tb_fungsionaris
+            $this->db->insert('tb_fungsionaris',$data);
                 $pesan='Data sudah disimpan';
                 $color='success';
-            }else{
-                $update=array(
-                    'id_fungsionaris'=>$id_fungsionaris
-                );
-                $this->db->where($update);
+            }
+ 		//jika ada id_fungsionaris lakukan update
+        else{
+			//update tb_fungsionaris dengan id_fungsionaris = $id_fungsionaris
+            $this->db->where('id_fungsionaris',$id_fungsionaris);
                 $this->db->update('tb_fungsionaris',$data);
                 $pesan='Data sudah diedit';
                 $color='success';
@@ -34,18 +40,24 @@
             redirect(base_url('cfungsionaris/fungsionaris/').$this->input->post('id_ukm'),'_self');
         }
     
-        public function edit_fungsionaris($id_fungsionaris){
-                $query = $this->db->get_where('tb_fungsionaris',['id_fungsionaris'=>$id_fungsionaris]);
+	//function buat mengisi data ke halaman form
+    public function edit_fungsionaris($id_fungsionaris){
+		//mengambil data ke tb_fungsionaris dari id_fungsionaris sama dengan parameter
+        $query = $this->db->get_where('tb_fungsionaris',['id_fungsionaris'=>$id_fungsionaris]);
                 if($query->num_rows()>0)
                 {
                     $data=$query->row();
-                    echo "<script>$('#id_fungsionaris').val('".$data->id_fungsionaris."')</script>";
+			//ajax untuk mengirim data ke form byid
+            echo "<script>$('#id_fungsionaris').val('".$data->id_fungsionaris."')</script>";
                     echo "<script>$('#id_mahasiswa').val('".$data->id_mahasiswa."')</script>";
                     echo "<script>$('#id_jabatan').val('".$data->id_jabatan."')</script>";
                     echo "<script>$('#status').val('".$data->status."')</script>";
                 }	
         }
-        public function delete_fungsionaris($id_ukm,$id_fungsionaris) {
+        
+	//function buat delete 
+    public function delete_fungsionaris($id_ukm,$id_fungsionaris) {
+    		//delete dari tb_fungsionaris dengan id_fungsionaris = $id_fungsionaris
             $this->db->where('id_fungsionaris',$id_fungsionaris);
             $this->db->delete('tb_fungsionaris');
             $query = $this->db->affected_rows();
